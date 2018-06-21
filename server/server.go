b9100d91c75a8a93ca815bbc45f80604bb6185d1
Callan Bryant <callan.bryant@gmail.com>
2018-06-21 22:29:30 +0100

    implement unary/stream type, fmt

--- server/server.go
+++ server/server.go
@@ -7,26 +7,19 @@ import "golang.org/x/net/context"
 import "google.golang.org/grpc"
 import pb "github.com/naggie/dspa5/proto"
 
-const port ":40401"
+const port = ":40401"
 
-type server struct{
+type server struct {
 	//synth chan
 }
 
-
-func (s *server) Speak(stream pb.Dspa5_SpeakServer) error {
+func (s *server) Speak(annoucement *pb.Announcement, stream pb.Dspa5_SpeakServer) error {
 	for {
-		point, err := stream.Recv()
-
-		if err == io.EOF {
-			return ...something
-		}
+		fmt.Printf("annoucement: %v\n", annoucement)
 
 		err = stream.Send(&pb.Announcement{"Hello", pb.Announcement_WARNING})
 	}
 }
-
-
 
 func main() {
 	path, ok := os.LookupEnv("DSPA_TTS_COMMAND")
