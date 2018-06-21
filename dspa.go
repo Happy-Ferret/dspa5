180325403e856d0d8ca3954ee4382ee7eac58bbd
Callan Bryant <callan.bryant@gmail.com>
2018-06-21 22:08:03 +0100

    start grpc integration

--- dspa.go
+++ dspa.go
@@ -2,6 +2,19 @@ package main
 
 import "os"
 import "fmt"
+import "golang.org/x/net/context"
+import "google.golang.org/grpc"
+import pb "github.com/naggie/dspa5/proto"
+
+const port ":40401"
+
+type server struct{}
+
+
+func (s *server) Speak(ctx context.Context, announcement *pb.Announcement) (*pb.Announcement, error) {
+
+}
+
 
 
 func main() {
