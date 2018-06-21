b6d3aecfbfe4d7444bc50181761a1d7f1c618af2
Callan Bryant <callan.bryant@gmail.com>
2018-06-21 22:26:27 +0100

    start Speak method

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import "os"
+import "io"
 import "fmt"
 import "golang.org/x/net/context"
 import "google.golang.org/grpc"
@@ -8,11 +9,21 @@ import pb "github.com/naggie/dspa5/proto"
 
 const port ":40401"
 
-type server struct{}
+type server struct{
+	//synth chan
+}
 
 
-func (s *server) Speak(ctx context.Context, announcement *pb.Announcement) (*pb.Announcement, error) {
+func (s *server) Speak(stream pb.Dspa5_SpeakServer) error {
+	for {
+		point, err := stream.Recv()
 
+		if err == io.EOF {
+			return ...something
+		}
+
+		err = stream.Send(&pb.Announcement{"Hello", pb.Announcement_WARNING})
+	}
 }
 
 
