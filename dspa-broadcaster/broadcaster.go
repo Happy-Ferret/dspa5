4e5f5c4fa8becf05b1dd41ac105fc1b53788775e
Callan Bryant <callan.bryant@gmail.com>
2018-07-11 21:11:20 +0100

    start implementation of broadcast mechanism

--- dspa-broadcaster/broadcaster.go
+++ dspa-broadcaster/broadcaster.go
@@ -15,4 +15,32 @@ import (
 func main() {
 	discoverer := sd01.NewDiscoverer('dspa5speaker')
 	discoverer.Start()
+
+	server := NewServer(discoverer)
+}
+
+type server struct {
+	// used to serialise announcements. Lock is used as insertion is
+	// nonblocking, easier than using channel.
+	announcementLock *sync.Mutex
+}
+
+func NewServer(discoverer sd01.Discoverer) *server {
+	return &server{
+		discoverer: discoverer,
+		announcementLock: &sync.Mutex{},
+	}
+}
+
+func (s *server) Speak(announcement pb.Announcement, stream pb.Dspa5_SpeakServer) error {
+	hosts := s.discoverer.GetServices()
+	replies := make(chan pbAnnouncement, 10)
+
+	for host := range hosts {
+		speakUpstream(host, announcement, replies)
+	}
+}
+
+func speakUpstream(host string, announcement pb.Announcement, replies chan<- pb.Announcement) {
+
 }
